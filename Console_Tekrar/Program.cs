100b9feddc934e57d76e2f82ee36dbed35184607
İsmail Oktay Dak <dakoktaytgb@gmail.com>
2022-11-19 13:23:32 +0300

    Add project files.

---
+++ Console_Tekrar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Tekrar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lütfen Adınızı giriniz");
+            string ad = Console.ReadLine();
+            Console.WriteLine("Lütfen Soyadınızı giriniz");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("Lütfen Yaşınızı giriniz");
+            int yas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Hoşgeldiniz adınız{ad}, soyadınız {soyad}, yaşınız {yas}.");
+
+
+        }
+    }
+}
