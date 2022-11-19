68f788a3ede0d99a6c5596a93c3593a2cd34d098
İsmail Oktay Dak <dakoktaytgb@gmail.com>
2022-11-19 13:38:20 +0300

    console introduction

---
+++ Console_Assigment_Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Assigment_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Atama Operatörü (=)
+
+            //Yazıldığı yerin sağındaki değeri , solundaki değişkene atar.
+            
+            string gelenVeri = Console.ReadLine();
+
+            //Her zaman string olarak gelir. Eğer int olması gerekiyorsa Convert.ToInt32() ile çeviririz.
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Console_Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Değişkenler (Variables)
+
+            //Değişkenler, programlama dillerinde verilerin saklanması için kullanılan isimlendirilmiş yerlerdir.
+
+            //Değişkenler verilerimizi ram üzerinde saklayan alanlardır.
+
+            //Değişken tanımlama syntaxı;
+
+            string ad;
+
+            //Değişkenler tanımlandıktan sonra değer atanmalıdır.
+            //Değişkenler tanımlanırken değer ataması yapılabilir.
+        }
+    }
+}
